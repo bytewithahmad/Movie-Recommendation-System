92748b4d5d26dd25d6102675a93a915df3a78103
bytewithahmad <ak8248823@gmail.com>
2026-03-17 02:07:03 +0530

    compute similarity dynamically

--- app.py
+++ app.py
@@ -20,8 +20,13 @@ img:hover {
 """, unsafe_allow_html=True)
 
 movies = pickle.load(open('movies.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
 API_KEY = "e4395dc7875b76938047d5ebef4f61d6"
 
 def fetch_poster(movie_id):
